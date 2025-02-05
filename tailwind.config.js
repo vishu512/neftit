7a2cc8513fa1f362d530d84bf0407b4f7387af48
vishu512 <70514375+vishu512@users.noreply.github.com>
2025-02-05 11:19:07 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#E4E4E5",
+        secondary: "#BEBEBE",
+        accent: "#39FF14",
+        dark1: "#3C3C3C",
+        dark2: "#545454",
+        gray1: "#949494",
+        gray2: "#7A7A7A"
+      },
+      backgroundImage: {
+        'gradient-dark': 'linear-gradient(to bottom, #000000, #0A1F3D)'
+      }
+    },
+  },
+  plugins: [],
+}
